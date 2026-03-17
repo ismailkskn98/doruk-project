364ed43071270932ef0bfa7de71e12009a7684b3
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-03-17 10:20:51 +0300

    Kod değişiklikleri yapıldı.

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,23 +3,39 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className='w-full fluid gridContainer'>
-            <main className='w-full fluid gridContainer bg-custom-gray'>
-                <section className='w-full flex items-end justify-between gap-5'>
-                    <article className='w-fit'></article>
-                    <article className='w-fit flex items-center gap-5 font-light'>
-                        <a href="https://www.instagram.com/dorukbicer/" target="_blank" rel="noopener noreferrer" className='text-sm font-bold uppercase'>
-                            Instagram
-                        </a>
-                        <a href="https://www.behance.net/dorukbicer" target="_blank" rel="noopener noreferrer" className='text-sm font-bold uppercase'>
-                            beHance
-                        </a>
-                        <a href="https://www.linkedin.com/in/dorukbicer/" target="_blank" rel="noopener noreferrer" className='text-sm font-bold uppercase'>
-                            LinkedIn
-                        </a>
+            <main className='w-full fluid gridContainer bg-custom-gray p-12.5 min-h-75'>
+                <section className='w-full h-full flex items-start justify-between gap-5'>
+                    <article className='w-fit h-full flex flex-col items-start justify-between'>
+                        <h2 className='text-[32px] font-bold font-forma-djr-display'><span className='font-light'>Studio</span> Doruk Bicer</h2>
+                        <div className='flex flex-col items-start gap-2.5 font-helvetica-neue font-light leading-4'>
+                            <a href="mailto:info@dorukbicer.com">info@dorukbicer.com</a>
+                            <p className=''>Via Giovanni Pastorelli 4 <br />Milan / Italy</p>
+                        </div>
+                    </article>
+                    <article className='w-fit h-full flex items-end'>
+                        <div className='flex items-center gap-5 font-light font-helvetica-neue'>
+                            <a href="https://www.instagram.com/dorukbicer/" target="_blank" rel="noopener noreferrer" className=''>
+                                Instagram
+                            </a>
+                            <a href="https://www.behance.net/dorukbicer" target="_blank" rel="noopener noreferrer" className=''>
+                                beHance
+                            </a>
+                            <a href="https://www.linkedin.com/in/dorukbicer/" target="_blank" rel="noopener noreferrer" className=''>
+                                LinkedIn
+                            </a>
+                        </div>
                     </article>
                 </section>
             </main>
-            <main>asdsa</main>
+            <main className='w-full py-2.5 flex items-center justify-center min-h-10 font-light font-helvetica-neue'>
+                <p>
+                    © Studio <span className='font-bold font-forma-djr-display'>Doruk Bicer</span> 2026 - All Rights Reserved
+                </p>
+                <span className='px-2.5'> | </span>
+                <p>Privacy Policy</p>
+                <span className='px-2.5'> | </span>
+                <p>Legal Notes</p>
+            </main>
         </footer>
     )
 }
