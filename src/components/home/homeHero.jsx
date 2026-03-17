640868711c279bf27f70b0d11bad3ee358cd03d2
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-03-17 09:52:29 +0300

    feat: homeHero componentinde video kaynağı güncellendi; gereksiz video dosyaları silindi.

--- src/components/home/homeHero.jsx
+++ src/components/home/homeHero.jsx
@@ -9,7 +9,7 @@ export default function HomeHero() {
             <Header />
             <main className='w-full flex items-center justify-center py-12.5 max-h-262 overflow-hidden'>
                 <video className='w-full h-full max-h-262.5 overflow-hidden' autoPlay loop muted>
-                    <source src="/videos/output-video.mp4" type="video/mp4" />
+                    <source src="/videos/home-hero-video.mp4" type="video/mp4" />
                 </video>
             </main>
             <Navbar />
