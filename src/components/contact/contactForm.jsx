e4879881d5479dd4ef39e72e57cbaaa9c30c9e16
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-03-18 09:47:56 +0300

    feat: ContactForm bileşeninde input ve textarea'lara padding eklendi.

--- src/components/contact/contactForm.jsx
+++ src/components/contact/contactForm.jsx
@@ -23,18 +23,18 @@ export default function ContactForm() {
             <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-start w-full max-w-125 gap-12'>
                 <article className='flex flex-col items-start gap-5 w-full'>
                     <label htmlFor="name" className='font-helvetica-neue font-bold text-2xl leading-4'>Name Surname:</label>
-                    <input {...register("name")} placeholder="" className='bg-custom-gray h-12.5 w-full' />
+                    <input {...register("name")} placeholder="" className='bg-custom-gray h-12.5 w-full p-3' />
                     {errors.name && <p>{errors.name.message}</p>}
                 </article>
 
                 <article className='flex flex-col items-start gap-5 w-full'>
                     <label htmlFor="email" className='font-helvetica-neue font-bold text-2xl leading-4'>Mail:</label>
-                    <input {...register("email")} placeholder="" className='bg-custom-gray h-12.5 w-full' />
+                    <input {...register("email")} placeholder="" className='bg-custom-gray h-12.5 w-full p-3' />
                     {errors.email && <p>{errors.email.message}</p>}
                 </article>
                 <article className='flex flex-col items-start gap-5 w-full'>
                     <label htmlFor="message" className='font-helvetica-neue font-bold text-2xl leading-4'>Text:</label>
-                    <textarea {...register("message")} placeholder="" className='bg-custom-gray h-12.5 w-full' />
+                    <textarea {...register("message")} placeholder="" className='bg-custom-gray w-full resize-none p-3' rows={10} />
                     {errors.message && <p>{errors.message.message}</p>}
                 </article>
                 <button type="submit" className='bg-custom-gray px-12 py-3 w-full font-bold text-2xl hover:bg-[#c6c6c6] transition-all duration-200 cursor-pointer'>Submit</button>
