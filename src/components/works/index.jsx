acb5b2a076d7cd09b7a6fd3a3676434aef47e7b7
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-03 09:47:32 +0300

    feat: WorksMain bileşeninde setTitle fonksiyonuna boş değer atandı.

--- src/components/works/index.jsx
+++ src/components/works/index.jsx
@@ -25,6 +25,7 @@ export default function WorksMain() {
 
   useEffect(() => {
     setLightTitle("WORKS");
+    setTitle("");
     setIntroComplete(true);
   }, [setTitle, setLightTitle, setIntroComplete]);
 
