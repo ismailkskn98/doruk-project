7c336f923a2203b58bf168f792e277fd3ff266ab
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-03 09:25:31 +0300

    feat: SideBar bileşenine kapatma butonu eklendi; SheetContent düzenlemeleri yapıldı.

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -3,6 +3,9 @@ import { useSidebarStore } from "@/store/sidebarStore";
 import React from "react";
 import SideNavbar from "./sideNavbar";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { Button } from "../ui/button";
+import { XIcon } from "lucide-react";
+import { DialogClose } from "../ui/dialog";
 
 export default function SideBar() {
   const sidebarOpen = useSidebarStore((state) => state.sidebarOpen);
@@ -13,8 +16,14 @@ export default function SideBar() {
       <SheetContent
         side="right"
         showCloseButton={false}
-        className="p-0 border-l border-gray-200 shadow-sm xl:w-[30%]! xl:max-w-[30%]! min-[1700px]:w-[calc((100vw-1500px)/2)]! min-[1700px]:max-w-[calc((100vw-1500px)/2)]!"
+        className="p-0 border-l border-gray-200 shadow-sm data-[side=right]:xl:w-[30%]! data-[side=right]:xl:max-w-[30%]! data-[side=right]:min-[131.25rem]:w-[calc((100vw-1500px)/2)]! data-[side=right]:min-[131.25rem]:max-w-[calc((100vw-1500px)/2)]!"
       >
+        <DialogClose asChild>
+          <Button variant="ghost" className="sm:hidden absolute top-3 right-3 cursor-pointer" size="icon-lg">
+            <XIcon />
+            <span className="sr-only">Close</span>
+          </Button>
+        </DialogClose>
         <SheetHeader className="hidden">
           <SheetTitle></SheetTitle>
           <SheetDescription></SheetDescription>
