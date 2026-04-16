f056c9abe3aa68131bc35ecff67e98264e0f8080
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-16 12:24:52 +0300

    refactor: GradientCircle bileşeninin yapısı güncellendi; metin ve stil iyileştirmeleri yapıldı, SVG kullanımı ile görsel hiyerarşi güçlendirildi. ExtreamityDetail bileşeninde stil tutarlılığı sağlandı ve metin düzenlemeleri yapıldı.

--- src/components/works/slug/extreamityDetail/gradientCircle.jsx
+++ src/components/works/slug/extreamityDetail/gradientCircle.jsx
@@ -1,31 +1,41 @@
 export default function GradientCircle() {
-    return (
-        <div className="relative h-full aspect-square w-full">
-            <div className="absolute left-1/2 top-0 -translate-x-1/2  text-[32px] font-light">
-                0
-            </div>
-            <div className="absolute bottom-0 left-1/2 -translate-x-1/2  text-[32px] font-light">
-                ∞
-            </div>
+  return (
+    <div className="relative w-full h-full">
+      <svg viewBox="0 0 620 620" className="relative w-full h-full">
+        <text
+          x="50%"
+          y="0%"
+          textAnchor="middle"
+          style={{
+            fontSize: '28px',
+            fontWeight: 200,
+          }}
+        >
+          0
+        </text>
 
-            <svg viewBox="0 0 620 620" className="h-full w-full">
-                <defs>
-                    <linearGradient id="circleGradient" x1="0%" y1="50%" x2="100%" y2="50%">
-                        <stop offset="0%" stopColor="#00ff00" />
-                        <stop offset="50%" stopColor="#2b0000" />
-                        <stop offset="100%" stopColor="#ff0000" />
-                    </linearGradient>
-                </defs>
+        <text
+          x="50%"
+          y="102%"
+          textAnchor="middle"
+          style={{
+            fontSize: '30px',
+            fontWeight: 200,
+          }}
+        >
+          ∞
+        </text>
 
-                <circle
-                    cx="310"
-                    cy="310"
-                    r="290"
-                    fill="none"
-                    stroke="url(#circleGradient)"
-                    strokeWidth="4"
-                />
-            </svg>
-        </div>
-    );
+        <defs>
+          <linearGradient id="circleGradient" x1="0%" y1="50%" x2="100%" y2="50%">
+            <stop offset="0%" stopColor="#00ff00" />
+            <stop offset="50%" stopColor="#2b0000" />
+            <stop offset="100%" stopColor="#ff0000" />
+          </linearGradient>
+        </defs>
+
+        <circle cx="310" cy="310" r="290" fill="none" stroke="url(#circleGradient)" strokeWidth="4" />
+      </svg>
+    </div>
+  );
 }
--- src/components/works/slug/extreamityDetail/index.jsx
+++ src/components/works/slug/extreamityDetail/index.jsx
@@ -1,10 +1,10 @@
-"use client";
-import CommonHero from "@/components/common/commonHero";
-import { useHeaderStore } from "@/store/headerStore";
-import { useIntroStore } from "@/store/introStore";
-import Image from "next/image";
-import React, { useEffect } from "react";
-import GradientCircle from "./gradientCircle";
+'use client';
+import CommonHero from '@/components/common/commonHero';
+import { useHeaderStore } from '@/store/headerStore';
+import { useIntroStore } from '@/store/introStore';
+import Image from 'next/image';
+import React, { useEffect } from 'react';
+import GradientCircle from './gradientCircle';
 
 export default function ExtreamityDetail() {
   const setTitle = useHeaderStore((state) => state.setTitle);
@@ -12,8 +12,8 @@ export default function ExtreamityDetail() {
   const setIntroComplete = useIntroStore((state) => state.setIntroComplete);
 
   useEffect(() => {
-    setLightTitle("Works");
-    setTitle("ART");
+    setLightTitle('Works');
+    setTitle('ART');
     setIntroComplete(true);
   }, [setTitle, setLightTitle, setIntroComplete]);
 
@@ -26,75 +26,93 @@ export default function ExtreamityDetail() {
         subtitle="Photograph Collection"
         description="The photography collection aims to make the viewer think about the possibility of opposite extremes to be the different paths to the same result, and question their goals. "
         sideInfo={[
-          { label: "DATE", value: "202" },
-          { label: "DIMENSION", value: "50 x 71cm" },
-          { label: "MATERIAL", value: "Color Print" },
+          { label: 'DATE', value: '202' },
+          { label: 'DIMENSION', value: '50 x 71cm' },
+          { label: 'MATERIAL', value: 'Color Print' },
         ]}
       />
 
-      <section className="fluid gridContainer w-full bg-custom-gray min-h-100 py-25 px-12.5">
+      {/* 
+      
+            <section className="fluid gridContainer w-full bg-custom-gray lg:min-h-100 py-12 sm:py-20 md:py-25 md:px-12.5">
         <main className="w-full flex items-center justify-end">
-          <article className="w-full flex flex-col items-start gap-4 max-w-187.5 font-minion-pro text-[20px] leading-6.5">
+          <article className="w-full max-w-170">
+            <p className="font-minion-pro text-base sm:text-lg lg:text-[20px] leading-6 sm:leading-7">
+              Home is; <br />A combination of objects and spaces. The only think that matters is the interaction of the human with the object. However, sometimes, the space is not enough for
+              a good interaction, and this is where this project begins to merge those spaces...
+            </p>
+          </article>
+        </main>
+      </section>
+      */}
+
+      <section className="fluid gridContainer w-full bg-custom-gray py-12 sm:py-20 md:py-25 md:px-12.5">
+        <main className="w-full flex items-center justify-end">
+          <article className="w-full flex flex-col items-start gap-4 max-w-187.5 font-minion-pro text-base sm:text-lg lg:text-[20px] leading-6 sm:leading-7">
             <p>
-              We live in a culture where we want always more and sometimes even the most of everything. Our goals are to be more even the most: healthy, wealthy, knowledgeable, safe and many others.
-              But extremes are same, too cold burs as same as too hot, just in a different way.
+              We live in a culture where we want always more and sometimes even the most of everything. Our goals are to be more even the most: healthy, wealthy, knowledgeable, safe and many
+              others. But extremes are same, too cold burs as same as too hot, just in a different way.
             </p>
             <p>
-              We strive to create homes where we can feel free, unrestrained but then we cover it high wall to and give up our freed in order to feel secure. And some, tuns it into a prison with a
-              context of a freedom.
+              We strive to create homes where we can feel free, unrestrained but then we cover it high wall to and give up our freed in order to feel secure. And some, tuns it into a prison
+              with a context of a freedom.
             </p>
             <p>
-              We exercise and eat healthy to stay well and live longer. What if we go too far? An American entrepreneur is spending million dollars a year, takes over 100 pills a day, and dedicates
-              6–7 hours daily to live longer. But is he truly living?
+              We exercise and eat healthy to stay well and live longer. What if we go too far? An American entrepreneur is spending million dollars a year, takes over 100 pills a day, and
+              dedicates 6–7 hours daily to live longer. But is he truly living?
             </p>
-            <p>What if both extremes are different paths to same result? Is everything is the same as nothing? Might positive infinity and negative infinity be the same end with different paths?</p>
+            <p>
+              What if both extremes are different paths to same result? Is everything is the same as nothing? Might positive infinity and negative infinity be the same end with different
+              paths?
+            </p>
           </article>
         </main>
       </section>
 
-      <section className="relative h-screen w-full fluid">
+      <section className="relative aspect-1460/973 max-h-[1000px] w-full fluid">
         <Image src="/images/projects/extreamity-detail-1.webp" alt="extreamity-detail-1" fill className="object-cover object-center w-full h-full" />
       </section>
 
-      <section className="grid grid-cols-2 gap-12 py-12 items-stretch">
-        <main className="flex flex-col gap-12.5">
-          <article className="flex flex-col items-end text-end gap-10">
-            <h3 className="font-helvetica-neue text-[24px] font-bold leading-10">Why Always More ?</h3>
+      <section className="grid xl:grid-cols-2 gap-8 lg:gap-12 py-8 lg:py-12 items-stretch">
+        <main className="order-2 xl:order-1 flex flex-col gap-9 2xl:gap-12.5">
+          <article className="flex flex-col items-end text-end gap-4 2xl:gap-10">
+            <h3 className="font-helvetica-neue text-lg lg:text-[24px] font-bold leading-7 2xl:leading-10">Why Always More ?</h3>
             <div className="flex flex-col items-end text-end">
-              <h4 className="font-helvetica-neue text-[20px] font-light">Needs & Satisfaction</h4>
-              <p className="font-minion-pro text-[20px] leading-7 ">
-                According to Maslow’s Hierarchy of Needs, once basic needs are met, the drive to achieve more continues as new goals emerge. Neurologically, this is linked to dopamine, which responds
-                more to the anticipation of rewards than to their fulfillment. However, when everything is easily attained, the expectation fades, dopamine production slows, and satisfaction
-                diminishes.
+              <h4 className="font-helvetica-neue text-base lg:text-lg 2xl:text-[20px] font-light">Needs & Satisfaction</h4>
+              <p className="font-minion-pro text-base lg:text-lg 2xl:text-[20px] leading-6 2xl:leading-7 ">
+                According to Maslow’s Hierarchy of Needs, once basic needs are met, the drive to achieve more continues as new goals emerge. Neurologically, this is linked to dopamine, which
+                responds more to the anticipation of rewards than to their fulfillment. However, when everything is easily attained, the expectation fades, dopamine production slows, and
+                satisfaction diminishes.
               </p>
             </div>
             <div className="flex flex-col items-end text-end -mt-5">
-              <h4 className="font-helvetica-neue text-[20px] font-light">Goal & Meaning</h4>
-              <p className="font-minion-pro text-[20px] leading-7 ">
-                The constant striving is concidered as searching for meaning in many psychologcal perspectives. But sine the satisfaction is short lived and new goals are set, people always sets new
-                goals to improve and develop their identitity. But whenthat person reachs all the goals, dissatisfaction and lost of meaning starts.
+              <h4 className="font-helvetica-neue text-base lg:text-lg 2xl:text-[20px] font-light">Goal & Meaning</h4>
+              <p className="font-minion-pro text-base lg:text-lg 2xl:text-[20px] leading-6 2xl:leading-7 ">
+                The constant striving is concidered as searching for meaning in many psychologcal perspectives. But sine the satisfaction is short lived and new goals are set, people always
+                sets new goals to improve and develop their identitity. But whenthat person reachs all the goals, dissatisfaction and lost of meaning starts.
               </p>
             </div>
           </article>
           <article className="flex flex-col items-start text-start gap-2.5">
-            <h3 className="font-helvetica-neue text-[24px] font-bold leading-10">Does Extreams Meet?</h3>
+            <h3 className="font-helvetica-neue text-lg lg:text-[24px] font-bold leading-7 2xl:leading-10">Does Extreams Meet?</h3>
             <div className="flex flex-col items-start text-start">
-              <h4 className="font-helvetica-neue text-[20px] font-light">Narcissism & Self-Hatred</h4>
-              <p className="font-minion-pro text-[20px] leading-7">
-                According to psychoanalysts such as Otto Kernberg, pathological narcissism and deep self-hatred may actually be two extremes of the same personality structure and they both concidered
-                as not being able to accept one’s true self and producing a mask.
+              <h4 className="font-helvetica-neue text-base lg:text-lg 2xl:text-[20px] font-light">Narcissism & Self-Hatred</h4>
+              <p className="font-minion-pro text-base lg:text-lg 2xl:text-[20px] leading-6 2xl:leading-7">
+                According to psychoanalysts such as Otto Kernberg, pathological narcissism and deep self-hatred may actually be two extremes of the same personality structure and they both
+                concidered as not being able to accept one’s true self and producing a mask.
               </p>
             </div>
             <div className="flex flex-col items-start text-start">
-              <h4 className="font-helvetica-neue text-[20px] font-light">Obsession & Submission</h4>
-              <p className="font-minion-pro text-[20px] leading-7">
-                In psychology they both are concidered as coming from the feeling of fear, uncertainty and insecurity. One reacts those emotions with the need of over controll and the other by staying
-                passive. Even they seems like a pollar siturations, they both are just different paths to same result in extreams with same the motivation in the beggining.
+              <h4 className="font-helvetica-neue text-lg 2xl:text-[20px] font-light">Obsession & Submission</h4>
+              <p className="font-minion-pro text-base lg:text-lg 2xl:text-[20px] leading-6 2xl:leading-7">
+                In psychology they both are concidered as coming from the feeling of fear, uncertainty and insecurity. One reacts those emotions with the need of over controll and the other
+                by staying passive. Even they seems like a pollar siturations, they both are just different paths to same result in extreams with same the motivation in the beggining.
               </p>
             </div>
           </article>
         </main>
-        <div className="h-full flex items-center justify-center min-w-188">
+        {/* min-w-188 */}
+        <div className="order-1 xl:order-2 w-full max-w-[756px] aspect-756/840">
           <GradientCircle />
         </div>
       </section>
