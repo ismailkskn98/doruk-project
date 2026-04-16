2594e2a0c1abaad5880a07063c1576f0d013224a
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-16 10:08:55 +0300

    refactor: Daha iyi okunabilirlik ve tutarlılık için ModularHomeDetail bileşeninin stilleri güncellendi; daha iyi görsel hiyerarşi için resim kaynakları ve düzeni ayarlandı.

--- src/components/works/slug/modularHomeDetail/index.jsx
+++ src/components/works/slug/modularHomeDetail/index.jsx
@@ -1,11 +1,11 @@
-"use client";
-import CommonHero from "@/components/common/commonHero";
-import MotionLeftView from "@/components/common/motionLeftView";
-import MotionRightView from "@/components/common/motionRightView";
-import { useHeaderStore } from "@/store/headerStore";
-import { useIntroStore } from "@/store/introStore";
-import Image from "next/image";
-import React, { useEffect } from "react";
+'use client';
+import CommonHero from '@/components/common/commonHero';
+import MotionLeftView from '@/components/common/motionLeftView';
+import MotionRightView from '@/components/common/motionRightView';
+import { useHeaderStore } from '@/store/headerStore';
+import { useIntroStore } from '@/store/introStore';
+import Image from 'next/image';
+import React, { useEffect } from 'react';
 
 export default function ModularHomeDetail() {
   const setTitle = useHeaderStore((state) => state.setTitle);
@@ -13,15 +13,15 @@ export default function ModularHomeDetail() {
   const setIntroComplete = useIntroStore((state) => state.setIntroComplete);
 
   useEffect(() => {
-    setLightTitle("Works");
-    setTitle("ART");
+    setLightTitle('Works');
+    setTitle('ART');
     setIntroComplete(true);
   }, [setTitle, setLightTitle, setIntroComplete]);
 
   return (
     <main className="w-full fluid gridContainer">
       <CommonHero
-        video="/videos/home-hero-video.mp4"
+        video="/videos/home-hero-video-2.mp4"
         title="The Modular Home"
         subtitle="Interior Concept"
         description="Living alone is a growing trend and when small spaces are combined
@@ -33,74 +33,71 @@ export default function ModularHomeDetail() {
                     the user to meet the current need. The ultimate ambition is to redefine
                     the environment as an extension of a human."
         sideInfo={[
-          { label: "DATE", value: "2025" },
-          { label: "DIMENSION", value: "7x7x3.5m" },
-          { label: "AWARDS", value: "iF Design Award 2026" },
+          { label: 'DATE', value: '2025' },
+          { label: 'DIMENSION', value: '7x7x3.5m' },
+          { label: 'AWARDS', value: 'iF Design Award 2026' },
         ]}
       />
       <section className="fluid gridContainer w-full bg-custom-gray lg:min-h-100 py-12 sm:py-20 md:py-25 md:px-12.5">
         <main className="w-full flex items-center justify-end">
           <article className="w-full max-w-170">
             <p className="font-minion-pro text-base sm:text-lg lg:text-[20px] leading-6 sm:leading-7">
-              Home is; <br />A combination of objects and spaces. The only think that matters is the interaction of the human with the object. However, sometimes, the space is not enough for a good
-              interaction, and this is where this project begins to merge those spaces...
+              Home is; <br />A combination of objects and spaces. The only think that matters is the interaction of the human with the object. However, sometimes, the space is not enough for
+              a good interaction, and this is where this project begins to merge those spaces...
             </p>
           </article>
         </main>
       </section>
-      <main className="w-full flex flex-col gap-18.75 py-12.5 lg:py-18.75">
+      <main className="w-full flex flex-col gap-12.5 sm:gap-15 lg:gap-18.75 py-12.5 lg:py-18.75">
         <section className="w-full">
           <article className="relative w-full aspect-1500/765">
             <Image src="/images/projects/the-modular-home-detail-1.webp" alt="The Modular Home Detail 1" fill className="object-cover w-full h-full" />
           </article>
         </section>
         <section className="w-full">
-          <article className="w-full grid grid-cols-2 place-content-stretch items-center gap-10">
-            <p className="text-[20px] font-minion-pro">
-              When small spaces are combined with traditional architecture, they often result limited functionality and cramped layouts. As a solution, the design coverts rooms into customizable smart
-              modules that transforms 49m2 meter space into different rooms. Instead of making the occupant move around the rooms, the projects aim to create the rooms around the user to meet the
-              current need. The ultimate ambition is to redefine the environment as an extension of a human.
+          <article className="w-full grid lg:grid-cols-2 place-content-stretch items-center gap-10">
+            <p className="order-2 lg:order-1 text-base sm:text-lg lg:text-[20px] font-minion-pro">
+              When small spaces are combined with traditional architecture, they often result limited functionality and cramped layouts. As a solution, the design coverts rooms into
+              customizable smart modules that transforms 49m2 meter space into different rooms. Instead of making the occupant move around the rooms, the projects aim to create the rooms
+              around the user to meet the current need. The ultimate ambition is to redefine the environment as an extension of a human.
             </p>
-            <div className="relative min-h-150 flex">
-              <MotionRightView className="relative w-100 h-100">
-                <Image src="/images/projects/the-modular-home-detail-2-lower.webp" alt="The Modular Home Detail 2 Lower" fill className="object-cover w-full h-full" />
+            <div className="relative order-1 lg:order-2 max-h-[660px] aspect-730/660 flex">
+              <MotionRightView className="relative w-full h-full">
+                <Image src="/images/projects/the-modular-home-detail-2-low.png" alt="The Modular Home Detail 2 Lower" fill className="object-cover w-full h-full" />
               </MotionRightView>
-              <MotionLeftView className="absolute bottom-0 right-0 w-100 h-100">
-                <Image src="/images/projects/the-modular-home-detail-2-high.webp" alt="The Modular Home Detail 2 High" fill className="object-cover w-full h-full" />
-              </MotionLeftView>
             </div>
           </article>
         </section>
-        <section className="w-full flex flex-col items-start gap-18.75">
+        <section className="w-full flex flex-col items-start gap-12.5 sm:gap-15 lg:gap-18.75">
           <article className="relative w-full max-w-222.25 h-125">
             <Image src="/images/projects/the-modular-home-detail-3.webp" alt="The Modular Home Detail 3" fill className="object-cover w-full h-full" />
           </article>
-          <article className="flex flex-col self-end items-start gap-3 max-w-213.5 font-minion-pro text-[20px] leading-7">
+          <article className="flex flex-col self-end items-start gap-3 max-w-213.5 font-minion-pro text-base sm:text-lg lg:text-[20px] leading-7">
             <p>
-              The project aims to re-design and update the living spaces to achieve more efficient utilization of space in a small apartment for a single occupant or a couple. The design based on the
-              idea of rooms can be treated as individual modules that can be opened and closed, depending on the occupant's current needs. The aim is to reduce the amount of unoccupied volumes while
-              also maximizing the usability of the available space.
+              The project aims to re-design and update the living spaces to achieve more efficient utilization of space in a small apartment for a single occupant or a couple. The design
+              based on the idea of rooms can be treated as individual modules that can be opened and closed, depending on the occupant's current needs. The aim is to reduce the amount of
+              unoccupied volumes while also maximizing the usability of the available space.
             </p>
             <p>
-              The concept is designed as a home that is 7-meter to a 7-meter single room, where 13 modular units share and transform to get maximum efficiency. The concept creates a flexible and
-              customizable living space which is three times more efficient than traditional architecture in small spaces. The design made to allow and adapt to the changing needs and preferences of
-              the occupants over time.
+              The concept is designed as a home that is 7-meter to a 7-meter single room, where 13 modular units share and transform to get maximum efficiency. The concept creates a flexible
+              and customizable living space which is three times more efficient than traditional architecture in small spaces. The design made to allow and adapt to the changing needs and
+              preferences of the occupants over time.
             </p>
           </article>
         </section>
-        <section className="w-full grid grid-cols-2 place-content-stretch gap-12.5">
-          <article className="relative w-full h-187.5">
+        <section className="w-full grid lg:grid-cols-2 place-content-stretch justify-items-stretch gap-12.5">
+          <article className="relative aspect-750/750">
             <Image src="/images/projects/the-modular-home-detail-4.webp" alt="The Modular Home Detail 4" fill className="object-cover w-full h-full" />
           </article>
-          <article className="flex flex-col items-start gap-10">
-            <video className="block w-full h-full max-h-118 bg-white object-fit" autoPlay loop muted>
+          <article className="flex h-full flex-col items-start gap-5 min-[86.25rem]:gap-10">
+            <video className="block aspect-725/472 bg-white object-fit" autoPlay loop muted>
               <source src="/videos/the-modular-home-detail-video.mp4" type="video/mp4" />
             </video>
-            <div className="flex flex-col items-start gap-10">
+            <div className="flex flex-col items-start gap-2 xl:gap-5 min-[86.25rem]:gap-10">
               <h3 className="text-[32px] font-helvetica-neue font-light">Kitchen</h3>
-              <p className="font-minion-pro text-[20px] leading-7">
-                The kitchen module functions in two modes: fully open as a traditional kitchen, or closed for basic use. It supports users by ordering food based on health data, with deliveries stored
-                in a temperature-controlled cabinet.
+              <p className="font-minion-pro text-base sm:text-lg xl:text-[20px] sm:leading-5 xl:leading-7">
+                The kitchen module functions in two modes: fully open as a traditional kitchen, or closed for basic use. It supports users by ordering food based on health data, with
+                deliveries stored in a temperature-controlled cabinet.
               </p>
             </div>
           </article>
