6f55adf8a33466843e56195163729e4b2e279a7e
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-17 16:10:50 +0300

    refactor: ContactMain bileşeninde stil ve düzen güncellemeleri yapıldı; duyarlı tasarım iyileştirildi ve bileşen yapısı güçlendirildi.

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,10 +1,10 @@
-"use client";
-import { useHeaderStore } from "@/store/headerStore";
-import { useIntroStore } from "@/store/introStore";
-import React, { useEffect } from "react";
-import MotionLeftView from "../common/motionLeftView";
-import MotionRightView from "../common/motionRightView";
-import ContactForm from "./contactForm";
+'use client';
+import { useHeaderStore } from '@/store/headerStore';
+import { useIntroStore } from '@/store/introStore';
+import React, { useEffect } from 'react';
+import MotionLeftView from '../common/motionLeftView';
+import MotionRightView from '../common/motionRightView';
+import ContactForm from './contactForm';
 
 export default function ContactMain() {
   const setTitle = useHeaderStore((state) => state.setTitle);
@@ -12,17 +12,17 @@ export default function ContactMain() {
   const setIntroComplete = useIntroStore((state) => state.setIntroComplete);
 
   useEffect(() => {
-    setLightTitle("Contact");
-    setTitle("");
+    setLightTitle('Contact');
+    setTitle('');
     setIntroComplete(true);
   }, [setTitle, setLightTitle, setIntroComplete]);
 
   return (
-    <main className="w-full flex flex-col items-start">
-      <MotionLeftView className="w-full pb-4 pt-8 lg:py-12.5">
+    <main className="w-full gridContainer">
+      <MotionLeftView className="w-full">
         <h1 className="uppercase font-bold text-xl sm:text-2xl lg:text-[32px] font-helvetica-neue">Contact</h1>
       </MotionLeftView>
-      <MotionRightView className="w-full flex items-start justify-between pt-4 pb-8 sm:pt-8 sm:pb-12.5 lg:py-12.5 font-helvetica-neue">
+      <MotionRightView className="w-full flex items-start justify-between py-[clamp(14px,4vw,50px)] font-helvetica-neue">
         <ContactForm />
       </MotionRightView>
     </main>
