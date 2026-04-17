dfbe1b31b7ea981e3c162e0d78864ebca79de811
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-17 16:37:38 +0300

    aaa

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: 'standalone',
+  // output: 'standalone',
 };
 
 export default nextConfig;
@@ -11,6 +11,20 @@ npm run build
 # statik dosyaları ekle
 cp -r public .next/standalone/
 cp -r .next/static .next/standalone/.next/
+
+# çalıştır
+node .next/standalone/server.js
+
+*/
+
+/*
+PwerShell ile build
+
+npm run build
+
+# statik dosyaları ekle
+Copy-Item public -Destination .next/standalone/public -Recurse
+Copy-Item .next/static -Destination .next/standalone/.next/static -Recurse
 
 # çalıştır
 node .next/standalone/server.js
