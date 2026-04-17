99b279e1f1c6d6c8d56d00a4b62ef54e7c957d0d
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-17 16:33:58 +0300

    chore: .gitignore dosyasını yarn.lock ve package-lock.json dosyalarını içerecek şekilde güncelleyin; next.config.mjs dosyasını bağımsız çıktı özelliğini etkinleştirecek ve derleme talimatlarını ekleyecek şekilde değiştirin.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  // output: 'standalone',
+  output: 'standalone',
 };
 
 export default nextConfig;
+/*
+npm run build
+
+# statik dosyaları ekle
+cp -r public .next/standalone/
+cp -r .next/static .next/standalone/.next/
+
+# çalıştır
+node .next/standalone/server.js
+
+*/
