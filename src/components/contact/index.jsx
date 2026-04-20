9253a9d59697ee17ec081ccfc2c7495679ab7cfb
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-20 11:39:18 +0300

    feat: ContactMain bileşenine resim eklendi; HomeHero bileşeninde video bağlantısı düzenlendi.

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -5,6 +5,7 @@ import React, { useEffect } from 'react';
 import MotionLeftView from '../common/motionLeftView';
 import MotionRightView from '../common/motionRightView';
 import ContactForm from './contactForm';
+import Image from 'next/image';
 
 export default function ContactMain() {
   const setTitle = useHeaderStore((state) => state.setTitle);
@@ -22,8 +23,14 @@ export default function ContactMain() {
       <MotionLeftView className="w-full">
         <h1 className="uppercase font-bold text-xl sm:text-2xl lg:text-[32px] font-helvetica-neue">Contact</h1>
       </MotionLeftView>
-      <MotionRightView className="w-full flex items-start justify-between py-[clamp(14px,4vw,50px)] font-helvetica-neue">
-        <ContactForm />
+      <MotionRightView className="w-full flex items-stretch gap-12.5 py-[clamp(14px,4vw,50px)] font-helvetica-neue">
+        <div className="w-full max-w-[500px] shrink-0">
+          <ContactForm />
+        </div>
+
+        <div className="hidden lg:block relative min-w-0 flex-1 overflow-hidden">
+          <Image src="/images/contact.png" alt="contact-side" fill className="object-cover object-center" />
+        </div>
       </MotionRightView>
     </main>
   );
--- src/components/home/homeHero.jsx
+++ src/components/home/homeHero.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Navbar from './navbar';
+import Link from 'next/link';
 
 export default function HomeHero() {
   return (
     <section className="w-full">
       <main className="w-full flex flex-col">
-        <div className="relative w-full overflow-hidden">
+        <Link href="/works/the-modular-home" className="relative block w-full overflow-hidden">
           <video className="w-full h-full lg:max-h-[800px]" autoPlay loop muted playsInline>
             <source src="/videos/home-hero-video-2.mp4" type="video/mp4" />
           </video>
-        </div>
+        </Link>
       </main>
       <Navbar />
     </section>
